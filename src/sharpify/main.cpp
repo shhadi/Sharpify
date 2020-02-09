5d0574608b12c4e10f976f1d5d58d8ec5c40d43b
Shhadi Masarwa <shhadi.masarwa@gmail.com>
2020-02-09 03:53:22 +0200

    Where function implemented for vector

--- src/sharpify/main.cpp
+++ src/sharpify/main.cpp
@@ -4,12 +4,29 @@
 #include "pch.h"
 
 #include "sharpify.h"
-
-
+#include<vector>
+//#include <algorithm>
 using namespace std;
 
 int main()
 {
-	vector<int> numbers;
-	numbers.where(NULL);
+	//vector<int> numbers = vector<int>();
+	//numbers.where(NULL);
+
+
+	vector<int> numbers = vector<int>();
+	numbers.push_back(1);
+	numbers.push_back(2);
+	numbers.push_back(3);
+	numbers.push_back(4);
+	numbers.push_back(5);
+	numbers.push_back(6);
+
+	auto bigNumbers = numbers.where([](int num) {return num > 3; });
+
+	for (auto num : bigNumbers)
+	{
+
+	}
+
 }
--- src/sharpify/sharpify.h
+++ src/sharpify/sharpify.h
@@ -1,5 +1,5 @@
-#ifndef _SHARPIFY_
-#define _SHARPIFY_
+#ifndef _SHARPIFY_H_
+#define _SHARPIFY_H_
 #include <vector>
 //New name for this file should be "sharpify"
 
@@ -7,13 +7,26 @@ namespace std
 {
 	template <class T>
 	class Vector : public std::vector<T>
-	{	
+	{
+	private:
+		vector<T> _matches;
 	public:
-		//template <class G>
-		void where(T predicate)
+
+		template <class G>
+		const vector<T>& where(const G& predicate)
 		{
-			auto result = find(this->begin(), this->end(), predicate);
+			vector<T> matches = vector<T>();
+
+			for (auto iterator = this->begin(); iterator != this->end(); ++iterator)
+			{
+				if (predicate(*iterator))
+				{
+					matches.push_back(*iterator);
+				}
+			}
+			return matches;
 		}
+
 	};
 }
 
